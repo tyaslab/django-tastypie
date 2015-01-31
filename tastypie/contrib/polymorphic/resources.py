e8488da7de940c6df58c9e006e70293592a131fc
tyaslab <tyaslab.com@gmail.com>
2015-01-31 10:56:39 +0700

    fixed polymorphic resource mixin

--- tastypie/contrib/polymorphic/resources.py
+++ tastypie/contrib/polymorphic/resources.py
@@ -1,84 +1,40 @@
-from django.core.urlresolvers import resolve
-from django.core.exceptions import ImproperlyConfigured
-from django.core.urlresolvers import get_script_prefix, Resolver404
-from django.db.models import get_model
-
-from tastypie.bundle import Bundle
 from tastypie.resources import ModelResource
-from tastypie.exceptions import NotFound
+from django.utils.importlib import import_module
+
+def load_variable(module_path):
+    module_bits = module_path.split('.')
+    module_path, the_variable = '.'.join(module_bits[:-1]), module_bits[-1]
+
+    i = import_module(module_path)
+    variable = getattr(i, the_variable)
+
+    return variable
 
 
 class PolymorphicModelResourceMixin(object):
-    class Meta:
-        polymorphic_resources = {}
+    def get_polymorphic_resource(self, bundle):
+        if type(bundle.obj) == self._meta.object_class:
+            return self
 
-    def get_polymorphic_resource(self, key):
-        return self._meta.polymorphic_resources[key]
+        resource_string = self._meta.polymorphic_resource[type(bundle.obj)]
 
-    def get_object_list(self, request, queryset=None):
-        # I don't know why I should do it
-        # But what I know is, when I replace with this, everything runs smoothly
-        # Problem?
-
-        queryset = self._meta.object_class.objects.all()
-
-        return super(PolymorphicModelResourceMixin, self).get_object_list(request, queryset=queryset)
+        return load_variable(resource_string)()
 
     def full_dehydrate(self, bundle, for_list=False):
-        real_model = bundle.obj.get_real_instance_class()
+        resource = self.get_polymorphic_resource(bundle)
 
-        is_parent_model = real_model == self._meta.object_class
+        if resource == self:
+            return super(PolymorphicModelResourceMixin, self).full_dehydrate(bundle, for_list=for_list)
 
-        if not is_parent_model:
-            key = '%s.%s' % (real_model._meta.app_label, real_model.__name__)
-            polymorphic_resource = self.get_polymorphic_resource(key)
+        return resource.full_dehydrate(bundle, for_list=for_list)
 
-            if bundle.obj is None:
-                bundle.obj = real_model()
+    def dehydrate_resource_uri(self, bundle):
+        resource = self.get_polymorphic_resource(bundle)
 
-            # set fields to appropriate resource
-            # but keep the original information so the original class still in use
-            # useful when for_list = True
-            self.original_fields = getattr(self, 'original_fields', self.fields)
-            self.original_resource_name = getattr(self, 'original_resource_name', self._meta.resource_name)
+        if resource == self:
+            return super(PolymorphicModelResourceMixin, self).dehydrate_resource_uri(bundle)
 
-            self.fields = polymorphic_resource.fields
-            self._meta.resource_name = polymorphic_resource._meta.resource_name
-        else:
-            self.fields = getattr(self, 'original_fields', self.fields)
-            self._meta.resource_name = getattr(self, 'original_resource_name', self._meta.resource_name)
-
-        return super(PolymorphicModelResourceMixin, self).full_dehydrate(bundle, for_list=for_list)
-
-    def get_via_uri(self, uri, request=None):
-        """
-        TODO: Must look for the best to it
-        """
-        resolved_uri = resolve(uri)
-        detail_uri_name = resolved_uri.kwargs[self._meta.detail_uri_name]
-
-        uri_kwargs = self.resource_uri_kwargs()
-        uri_kwargs[self._meta.detail_uri_name] = detail_uri_name
-        uri = self._build_reverse_url('api_dispatch_detail', kwargs=uri_kwargs)
-
-        return super(PolymorphicModelResourceMixin, self).get_via_uri(uri, request=request)
-
-    def build_filters(self, filters=None):
-        injected_filter = {}
-        inject = False
-        if filters.get('polymorphic_ctype_id', None):
-            inject = True
-            injected_filter = {
-                'polymorphic_ctype_id': filters['polymorphic_ctype_id']
-            }
-
-        filters = super(PolymorphicModelResourceMixin, self).build_filters(filters=filters)
-
-        if inject:
-            filters.update(injected_filter)
-
-        return filters
+        return resource.dehydrate_resource_uri(bundle)
 
 class PolymorphicModelResource(PolymorphicModelResourceMixin, ModelResource):
-    class Meta(PolymorphicModelResourceMixin.Meta):
-        pass
+    pass
